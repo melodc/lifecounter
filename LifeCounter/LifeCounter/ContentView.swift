900c67625ad3098ca9e68542189be39ae2fbe65f
melodc <melodc@uw.edu>
2021-04-21 15:21:48 -0700

    Fix padding issues and add screenshots

--- LifeCounter/LifeCounter/ContentView.swift
+++ LifeCounter/LifeCounter/ContentView.swift
@@ -18,13 +18,12 @@ struct ContentView: View {
         return GeometryReader { geo in
             if geo.size.width > geo.size.height {
                 VStack(alignment: .center, spacing: 0)  {
-                    Text("Life Counter Landscape")
-                        .font(.title)
-                    
+                    Text("Life Counter")
+                        .font(.title).padding()
                     HStack {
                         VStack {
                             Text(p1count > 1 || p1count == 0 ? "Player 1: \(p1count) lives" : "Player 1: \(p1count) life")
-                                .font(.title2).padding()
+                                .font(.title2)
                             
                             HStack {
                                 Button(action: {
@@ -59,7 +58,7 @@ struct ContentView: View {
                     
                         VStack {
                             Text(p2count > 1 || p2count == 0 ? "Player 2: \(p2count) lives" : "Player 2: \(p2count) life")
-                                .font(.title2).padding()
+                                .font(.title2)
                             
                             HStack {
                                 Button(action: {
@@ -97,7 +96,7 @@ struct ContentView: View {
                     HStack {
                         VStack {
                             Text(p3count > 1 || p3count == 0 ? "Player 3: \(p3count) lives" : "Player 3: \(p3count) life")
-                                .font(.title2).padding()
+                                .font(.title2)
                             
                             HStack {
                                 Button(action: {
@@ -132,7 +131,7 @@ struct ContentView: View {
                         
                         VStack {
                             Text(p4count > 1 || p4count == 0 ? "Player 4: \(p4count) lives" : "Player 4: \(p4count) life")
-                                .font(.title2).padding()
+                                .font(.title2)
                             
                             HStack {
                                 Button(action: {
@@ -169,13 +168,13 @@ struct ContentView: View {
                         
                     HStack {
                         if p1count <= 0 {
-                            Text("Player 1 LOSES!").font(.title).padding()
+                            Text("Player 1 LOSES!").font(.title)
                         } else if p2count <= 0 {
-                            Text("Player 2 LOSES!").font(.title).padding()
+                            Text("Player 2 LOSES!").font(.title)
                         } else if p3count <= 0 {
-                            Text("Player 3 LOSES!").font(.title).padding()
+                            Text("Player 3 LOSES!").font(.title)
                         } else if p4count <= 0 {
-                            Text("Player 4 LOSES!").font(.title).padding()
+                            Text("Player 4 LOSES!").font(.title)
 
                         }
                     }
@@ -184,13 +183,13 @@ struct ContentView: View {
                 .frame(width: geo.size.width, height: geo.size.height)
                 
             } else {
-                VStack {
-                    Text("Life Counter Portrait")
+                VStack(alignment: .center, spacing: 0) {
+                    Text("Life Counter")
                         .font(.title).padding()
-                    
+    
                     VStack {
                         Text(p1count > 1 || p1count == 0 ? "Player 1: \(p1count) lives" : "Player 1: \(p1count) life")
-                            .font(.title2).padding()
+                            .font(.title2)
                         
                         HStack {
                             Button(action: {
@@ -222,11 +221,10 @@ struct ContentView: View {
                             .buttonStyle(BlueButton())
                         }
                     }
-                    .padding()
                     
                     VStack {
                         Text(p2count > 1 || p2count == 0 ? "Player 2: \(p2count) lives" : "Player 2: \(p2count) life")
-                            .font(.title2).padding()
+                            .font(.title2)
                         
                         HStack {
                             Button(action: {
@@ -260,11 +258,10 @@ struct ContentView: View {
                         }
                         
                     }
-                    .padding()
                     
                     VStack {
                         Text(p3count > 1 || p3count == 0 ? "Player 3: \(p3count) lives" : "Player 3: \(p3count) life")
-                            .font(.title2).padding()
+                            .font(.title2)
                         
                         HStack {
                             Button(action: {
@@ -296,11 +293,10 @@ struct ContentView: View {
                             .buttonStyle(BlueButton())
                         }
                     }
-                    .padding()
                     
                     VStack {
                         Text(p4count > 1 || p4count == 0 ? "Player 4: \(p4count) lives" : "Player 4: \(p4count) life")
-                            .font(.title2).padding()
+                            .font(.title2)
                         
                         HStack {
                             Button(action: {
@@ -332,16 +328,15 @@ struct ContentView: View {
                             .buttonStyle(BlueButton())
                         }
                     }
-                    .padding()
              
                     if p1count <= 0 {
-                        Text("Player 1 LOSES!").font(.title).padding()
+                        Text("Player 1 LOSES!").font(.title)
                     } else if p2count <= 0 {
-                        Text("Player 2 LOSES!").font(.title).padding()
+                        Text("Player 2 LOSES!").font(.title)
                     } else if p3count <= 0 {
-                        Text("Player 3 LOSES!").font(.title).padding()
+                        Text("Player 3 LOSES!").font(.title)
                     } else if p4count <= 0 {
-                        Text("Player 4 LOSES!").font(.title).padding()
+                        Text("Player 4 LOSES!").font(.title)
 
                     }
                     
